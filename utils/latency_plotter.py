1bdf1e7562f836a58728b998e1986518c5316b3e
Elisha Lai <elisha.lai@outlook.com>
2017-07-01 16:04:51 -0400

    Add utility scripts for plotting results

---
+++ utils/latency_plotter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+# =============================================================================
+#  HornetQ-Tests
+#
+#  @author: Elisha Lai
+#  @description: Utility script to plot the latencies
+#  @version: 0.0.1 30/10/2016
+# =============================================================================
+
+
+def plot_cdf():
+    pass
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
