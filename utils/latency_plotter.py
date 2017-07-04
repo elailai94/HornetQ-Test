aa3cb7f897d99a91544eed6a7e0a74513cec5631
Elisha Lai <elisha.lai@outlook.com>
2017-07-04 11:49:07 -0400

    Improve utitily scripts

--- utils/latency_plotter.py
+++ utils/latency_plotter.py
@@ -5,17 +5,26 @@
 #
 #  @author: Elisha Lai
 #  @description: Utility script to plot the latencies
-#  @version: 0.0.1 30/10/2016
+#  @version: 1.0 27/06/2017
 # =============================================================================
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 
+# Plot histogram of latencies
+# NOTE: Need to fix this
 def plot_histogram(data):
-    pass
+    num_messages, latency = np.histogram(data, bins='auto')
+    num_messages_cumulative = np.cumsum(num_messages)
+    plt.title("Histogram of HornetQ End-to-End Latency")
+    plt.xlabel("Latency (ms)")
+    plt.ylabel("Number of Messages")
+    plt.plot(latency[:1], num_messages_cumulative)
+    plt.show()
 
 
+# Plot cumulative distribution function of latencies
 def plot_cdf(data):
     sorted_data = np.sort(data)
     num_data_points = data.size
@@ -31,14 +40,7 @@ def main():
     # Load data from the CSV file with the latencies
     latencies = np.loadtxt("latency.csv", delimiter=",", skiprows=1, usecols=3)
     plot_cdf(latencies)
-    #p = 1. * arange(len(latencies)) / (len(latencies) - 1)
-    #plt.plot(latencies_sorted, p, c='blue')
-    # Compute histogram of the set of data
-    #num_messages, latency = np.histogram(latencies, bins='auto', density=True)
-    # Compute cumulative sum of
-    #num_messages_cumulative = np.cumsum(num_messages)
-    # Plot
-    #plt.plot(latency[:-1], num_messages_cumulative, c='blue')
+    #plot_histogram(latencies)
 
 
 if __name__ == "__main__":
